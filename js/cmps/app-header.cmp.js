59a3086699fe5c6fb817260e8e355889ef926229
dorrang <summ.pie@gmail.com>
2021-02-24 17:55:04 +0200

    main page

--- js/cmps/app-header.cmp.js
+++ js/cmps/app-header.cmp.js
@@ -2,7 +2,7 @@ export default {
     template: `
    <header class="app-header">
        <div class="logo">
-           <h1>Books</h1>
+           <h1>AppSus</h1>
        </div>
        <nav>
            <router-link active-class="active-link" to="/" exact class="nav-link">Home</router-link> 
--- js/pages/appsus.cmp.js
+++ js/pages/appsus.cmp.js
@@ -3,13 +3,12 @@ export default {
     <section class="home app-main">
     <div class="round"></div>
 
-           <div class="story">
-         <h1>Think about the last good book you read.</h1>
-    <h2> Did it make you feel more connected to others?
-    <br> Maybe it served as a welcome escape.
-    <br> Maybe it helped you rediscover the beauty in life.
-    <br> Did it surprise you?</h2>
-    <h3>We are here to provide for you the BEST litrature, amazing stories & novels.</h3>
+        <div class="story">
+            <h1>All good things in hand.</h1>
+            <h2> Did it make you feel more connected to others?
+            <br> Maybe it served as a welcome escape.
+            <br> Maybe it helped you rediscover the beauty in life.
+            <br> Did it surprise you?</h2>
         </div>
     </section>
     `
