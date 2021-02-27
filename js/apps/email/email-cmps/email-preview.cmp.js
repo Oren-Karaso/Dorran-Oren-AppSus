0a794e52e3907c20d377b4218ab42bc8302756fa
Oren Karaso <orenkaraso@gmail.com>
2021-02-27 14:00:44 +0200

    someore css and inner preview container of email changes

--- js/apps/email/email-cmps/email-preview.cmp.js
+++ js/apps/email/email-cmps/email-preview.cmp.js
@@ -2,8 +2,9 @@ export default {
     props: ['email'],
     template: `
     <section class="email-preview">
-        <p class ="from">From: {{email.content.from }} <span>{{showEmailAddress}}</span></p>
+        <!-- <p class ="from">{{email.content.from }} <span>{{showEmailAddress}}</span></p> -->
         <p class="subject">Subject: <span >{{email.content.subject}}</span></p>
+        <p class="content">Content: <span >{{email.content.msgBody}}</span></p>
     </section>
     `,
     data() {
